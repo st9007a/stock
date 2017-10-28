5fe10890fa4a0f6db3a4712d728767792c4e7002
uno <st9007a@gmail.com>
2017-10-28 10:43:52 +0800

    Modularize config

--- eval_nn.py
+++ eval_nn.py
@@ -18,8 +18,8 @@ model.add(Dense(2, activation = 'softmax'))
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics = ['accuracy'])
 
-x_tr, y_tr = load_data('2017-07-01', '2017-10-20', onehot = True)
-x_te, y_te = pick_validate_data('2017-10-25', onehot = True)
+x_tr, y_tr = load_data(config['start'], config['end'], onehot = True)
+x_te, y_te = pick_validate_data(config['eval'], onehot = True)
 
 model.fit(x_tr, y_tr, epochs = 20, batch_size = 128, validation_split = 0.2, verbose = 1)
 
--- nn.py
+++ nn.py
@@ -20,23 +20,15 @@ model.add(Dense(2, activation = 'softmax'))
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics = ['accuracy'])
 
-x_tr, y_tr = load_data('2017-07-01', '2017-10-20', onehot = True)
-# x_te, y_te = pick_validate_data('2017-10-25', onehot = True)
+x_tr, y_tr = load_data(config['start'], config['end'], onehot = True)
 x_te, com = load_pred_data(date)
 
 model.fit(x_tr, y_tr, epochs = 20, batch_size = 128, validation_split = 0.2, verbose = 1)
-
-# score = model.evaluate(x_te, y_te)
-#
-# print('')
-# print('predict loss: ' + str(score[0]))
-# print('predict acc: ' + str(score[1]))
 
 proba = model.predict(x_te)
 stocks = []
 final_com = []
 final_x = []
-# y_f = []
 
 i = 0
 for p in proba:
@@ -44,7 +36,6 @@ for p in proba:
         stocks.append(p)
         final_com.append(com[i])
         final_x.append(x_te[i])
-        # y_f.append(y_te[i])
 
     i += 1
 
