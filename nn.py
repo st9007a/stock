8f31be8553d048ac4292277d0a17854823adc504
uno <st9007a@gmail.com>
2017-10-28 11:02:45 +0800

    Generate list of disicion file

--- nn.py
+++ nn.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import sys
 import numpy as np
@@ -8,9 +9,54 @@ from keras.layers import Dense, Activation, BatchNormalization, LSTM
 
 from util.dataset import load_data, pick_validate_data, load_pred_data
 
+def select_stock(model, date):
+    x_te, com = load_pred_data(date)
+    proba = model.predict(x_te)
+
+    final_y = []
+    final_com = []
+    final_x = []
+
+    i = 0
+    for p in proba:
+        if (p[0] > config['up_lower_bound'] and p[0] < config['up_upper_bound']) or (p[1] > config['down_lower_bound'] and p[1] < config['down_upper_bound']):
+            final_y.append(p)
+            final_com.append(com[i])
+            final_x.append(x_te[i])
+
+        i += 1
+
+    return final_x, final_y, final_com
+
+def gen_dis_file(final_x, final_y, final_com, date):
+    disicion_list = []
+    i = 0
+    for xi in final_x:
+        type = None
+        if final_y[i][0] > final_y[i][1]:
+            type = 'buy'
+        else:
+            type = 'short'
+
+        disicion = {
+            'code': final_com[i],
+            'type': type,
+            'weigth': 1,
+            'life': 3,
+            'open_price': 'open',
+            'close_high_price': xi[1] + 0.1,
+            'close_low_price': xi[1] - 0.1
+        }
+        disicion_list.append(disicion)
+        i += 1
+
+    with open('../commit/' + date + '_' + date + '.json', 'w')as j:
+        json.dump(disicion_list, j)
+
 config = json.load(open('config.json', 'r'))
 
-date = sys.argv[1]
+date_from = sys.argv[1]
+date_to = sys.argv[2]
 
 model = Sequential()
 model.add(Dense(256, input_dim = 30))
@@ -21,45 +67,19 @@ model.add(Dense(2, activation = 'softmax'))
 model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics = ['accuracy'])
 
 x_tr, y_tr = load_data(config['start'], config['end'], onehot = True)
-x_te, com = load_pred_data(date)
 
 model.fit(x_tr, y_tr, epochs = 20, batch_size = 128, validation_split = 0.2, verbose = 1)
 
-proba = model.predict(x_te)
-stocks = []
-final_com = []
-final_x = []
-
 i = 0
-for p in proba:
-    if (p[0] > config['up_lower_bound'] and p[0] < config['up_upper_bound']) or (p[1] > config['down_lower_bound'] and p[1] < config['down_upper_bound']):
-        stocks.append(p)
-        final_com.append(com[i])
-        final_x.append(x_te[i])
+while(True):
+    d = (datetime.datetime.strptime(date_from, '%Y-%m-%d') + datetime.timedelta(days = i)).strftime('%Y-%m-%d')
+    if (datetime.datetime.strptime(date_to, '%Y-%m-%d') - datetime.datetime.strptime(d, '%Y-%m-%d')).total_seconds() < 0:
+        break
 
     i += 1
 
-disicion_list = []
-i = 0
-for xi in final_x:
-    type = None
-    if stocks[i][0] > stocks[i][1]:
-        type = 'buy'
-    else:
-        type = 'short'
+    final_x, final_y, final_com = select_stock(model, d)
+    gen_dis_file(final_x, final_y, final_com, d)
 
-    disicion = {
-        'code': final_com[i],
-        'type': type,
-        'weigth': 1,
-        'life': 3,
-        'open_price': 'open',
-        'close_high_price': xi[1] + 0.1,
-        'close_low_price': xi[1] - 0.1
-    }
-    disicion_list.append(disicion)
-    i += 1
 
-with open('../commit/' + date + '_' + date + '.json', 'w')as j:
-    json.dump(disicion_list, j)
 # model.save('nn_model.h5')
